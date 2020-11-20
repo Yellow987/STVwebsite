e7de2c56c454462c7edfd6055c1e35b2a704d423
Yellow987 <daryldsouza123@gmail.com>
2020-11-20 10:32:32 -0500

    src files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import HomepageImage from './components/HomepageImage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <HomepageImage />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React uwu YUS owo awa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
