6904032b2550e1b9c064cbf691b8000bcb703d95
Daryl D'Souza <daryldsouza123@gmail.com>
2020-11-21 15:09:09 -0500

    uwu

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React uwu YUS owo awa navBAR yesh
+          Learn React uwu YUS owo awa navBAR yeshsir
         </a>
       </header>
     </div>
