aad9c59f069146c115e6b2af0c9bce764e1e2a53
Daryl D'Souza <daryldsouza123@gmail.com>
2020-11-21 15:30:16 -0500

    12:50

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-import HomepageImage from './components/HomepageImage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <HomepageImage />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React uwu YUS owo awa navBAR yeshsir
-        </a>
-      </header>
+     hi uwu
     </div>
   );
 }
